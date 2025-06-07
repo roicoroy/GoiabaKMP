33877ae36a4455280bf096799f612430670bf576
Ricardo Watanabe Bento <roicoroy@yahoo.com.br>
2025-06-07 08:35:17 +0100

    Updated Consts.kt

--- data/src/commonMain/kotlin/com/goiaba/data/networking/Consts.kt
+++ data/src/commonMain/kotlin/com/goiaba/data/networking/Consts.kt
@@ -1,5 +1,5 @@
 package com.goiaba.data.networking
 
 var strapiAccessToken = "5311b5b7fb4b19b98ad66758d149a63ba82f5bf56310cf2e355347ecc1d0583560380e2dfbc2d3941c463c7242418ed3c0b4371211f450b24a462deae6bc3b54efd79af7a321485368b895a921f1acc4e883c22d966ca185a3d0ecb68bc5bc07bfa73dd2b3b0d57d7ac224ffa83208bb79daf270ab0238076d4f84b14f37170c"
-var straiUrl = "https://c846-2a00-23c7-dc88-f401-498b-5983-4562-20e7.ngrok-free.app/"
+var straiUrl = "https://bdcf-2a00-23c7-dc88-f401-c810-e230-2b26-2e8d.ngrok-free.app/"
 var posts = "api/posts?populate=*"
